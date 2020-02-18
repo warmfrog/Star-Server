955d3b6517f6be777eef69746b5a39fae381d409
libo <1594914459@qq.com>
2020-02-18 15:43:23 +0800

    添加测试用例

---
+++ src/test/java/TestAlbum.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author libo
+ * @date 2020/2/18
+ */
+public class TestAlbum {
+    @Test
+    void testA() {
+        System.out.println("Test Success");
+    }
+}
